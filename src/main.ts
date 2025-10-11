e93ef95c00a86630f8a0e0b9565fb51f55fa21fc
Maxim R <79020992+foxcaulfield@users.noreply.github.com>
2025-10-11 08:47:35 +0200

    Feature/nestjs init (#1)
    * feat: initialize NestJS application
    
    * feat: update configuration files
    
    * refactor: standardize code formatting

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+async function bootstrap(): Promise<void> {
+	const app = await NestFactory.create(AppModule);
+	await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap().catch((err): void => {
+	console.error(err);
+	process.exit(1);
+});
