edd90efa7d9f9687980508df2e74ff9f69a512c8
foxcaulfield <fox.caulfield@gmail.com>
2025-10-11 07:55:11 +0200

    feat: update configuration files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,55 @@
 // @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-  {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
-    },
-  },
+	{
+		ignores: ["eslint.config.mjs"],
+	},
+	eslint.configs.recommended,
+	...tseslint.configs.recommendedTypeChecked,
+	eslintPluginPrettierRecommended,
+	{
+		languageOptions: {
+			globals: {
+				...globals.node,
+				...globals.jest,
+			},
+			sourceType: "commonjs",
+			parserOptions: {
+				projectService: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+	},
+	{
+		rules: {
+			"@typescript-eslint/no-explicit-any": "off",
+			"@typescript-eslint/no-floating-promises": "warn",
+			"@typescript-eslint/no-unsafe-argument": "warn",
+			"prettier/prettier": [
+				"error",
+				{
+					singleQuote: false,
+					trailingComma: "es5",
+					useTabs: true,
+					tabWidth: 4,
+					printWidth: 120,
+				},
+			],
+			"@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "explicit" }],
+			"@typescript-eslint/explicit-function-return-type": [
+				"error",
+				{
+					allowExpressions: false,
+					allowTypedFunctionExpressions: false,
+					allowHigherOrderFunctions: false,
+					allowDirectConstAssertionInArrowFunctions: false,
+					allowConciseArrowFunctionExpressionsStartingWithVoid: false,
+				},
+			],
+		},
+	}
 );
