7573099fcbd1024aac98c6fddf098189378fa7e9
foxcaulfield <fox.caulfield@gmail.com>
2025-10-12 20:34:13 +0200

    refactor: update user model and authentication schema, removing obsolete fields and adding new ones

---
+++ prisma/migrations/20251012182733_switch_ids_to_text/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - The primary key for the `users` table will be changed. If it partially fails, the table could be left without primary key constraint.
+
+*/
+-- DropForeignKey
+ALTER TABLE "public"."account" DROP CONSTRAINT "account_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "public"."session" DROP CONSTRAINT "session_userId_fkey";
+
+-- AlterTable
+ALTER TABLE "account" ALTER COLUMN "userId" DROP DEFAULT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "session" ALTER COLUMN "userId" DROP DEFAULT,
+ALTER COLUMN "userId" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "users" DROP CONSTRAINT "users_pkey",
+ALTER COLUMN "id" DROP DEFAULT,
+ALTER COLUMN "id" SET DATA TYPE TEXT,
+ADD CONSTRAINT "users_pkey" PRIMARY KEY ("id");
+
+-- AddForeignKey
+ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "account" ADD CONSTRAINT "account_userId_fkey" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
