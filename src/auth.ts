8bac2965011882b3b569a4912db880c9be631d77
foxcaulfield <fox.caulfield@gmail.com>
2025-10-15 22:19:36 +0200

    feat: add SignUpHook to handle authentication path validation

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,9 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient, UserRole } from "@prisma/client";
+// import { th } from "date-fns/locale";
 // import { PrismaClient } from "generated/prisma";  /* Alt path, depends on your 'client' -> 'output' value in your schema.prisma file */
 
 const prismaClient = new PrismaClient().$extends({
@@ -20,7 +22,16 @@ const prismaClient = new PrismaClient().$extends({
 });
 export const auth = betterAuth({
 	secret: process.env.BETTER_AUTH_SECRET,
-
+	hooks: {
+		// before: async (ctx): Promise<undefined | Response> => {
+		// 	const allowedPaths = ["/sign-in/email", "/sign-up", "/get-session"];
+		// 	if ("path" in ctx && typeof ctx.path === "string" && allowedPaths.includes(ctx.path)) {
+		// 		await Promise.resolve();
+		// 		return;
+		// 	}
+		// 	throw new Error("Wrong auth path");
+		// },
+	},
 	database: prismaAdapter(prismaClient, {
 		provider: "postgresql",
 	}),
