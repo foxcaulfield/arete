1544bc0ab06a736274b4e57c36fb8805bfc444ec
foxcaulfield <fox.caulfield@gmail.com>
2025-10-20 15:59:31 +0200

    feat: update environment configuration and improve auth settings

--- src/auth.ts
+++ src/auth.ts
@@ -2,35 +2,21 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient, UserRole } from "@prisma/client";
-// import { th } from "date-fns/locale";
-// import { PrismaClient } from "generated/prisma";  /* Alt path, depends on your 'client' -> 'output' value in your schema.prisma file */
 
 const prismaClient = new PrismaClient().$extends({
 	query: {
 		user: {
 			create({ args, query }) {
 				// Ensure role is always set to 'USER' on creation
-				// if (args.data) {
 				args.data.role = UserRole.USER;
 				args.data.emailVerified = false;
-				// }
 				return query(args);
 			},
 		},
 	},
 });
 export const auth = betterAuth({
-	secret: process.env.BETTER_AUTH_SECRET,
-	hooks: {
-		// before: async (ctx): Promise<undefined | Response> => {
-		// 	const allowedPaths = ["/sign-in/email", "/sign-up", "/get-session"];
-		// 	if ("path" in ctx && typeof ctx.path === "string" && allowedPaths.includes(ctx.path)) {
-		// 		await Promise.resolve();
-		// 		return;
-		// 	}
-		// 	throw new Error("Wrong auth path");
-		// },
-	},
+	secret: process.env.BETTER_AUTH_SECRET as string,
 	database: prismaAdapter(prismaClient, {
 		provider: "postgresql",
 	}),
@@ -38,8 +24,7 @@ export const auth = betterAuth({
 	emailAndPassword: {
 		enabled: true,
 	},
-	advanced: { cookiePrefix: "arete" },
-	/* Settings that will be added to session data (client-side) */
+	advanced: { cookiePrefix: process.env.COOKIE_PREFIX || "arete" },
 	// user: {
 	// 	additionalFields: {
 	// 		role: {
@@ -48,7 +33,5 @@ export const auth = betterAuth({
 	// 		},
 	// 	},
 	// },
-
-	// Frontend origin
-	trustedOrigins: ["http://localhost:5173"],
+	trustedOrigins: [process.env.FRONTEND_ORIGIN || "http://localhost:5173"],
 });
